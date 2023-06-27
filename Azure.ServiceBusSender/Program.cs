ec5ab2a38c6d404459de860cc212b91773ae0ffe
Henry Megwai <henry.megwai@venturegardengroup.com>
2023-06-27 09:43:27 +0200

    Add project files.

---
+++ Azure.ServiceBusSender/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "put your connection string here";
+
+const string queueName = "your-queuena you can always me-here";
+const int maxNumberOfMessages = 3; // you can always cha=nge this your preferred max number
+
+ServiceBusClient serviceBusClient;
+ServiceBusSender serviceBusSender;
+
+try
+{
+    serviceBusClient = new ServiceBusClient(serviceBusConnectionString);
+    serviceBusSender = serviceBusClient.CreateSender(queueName);
+
+    using ServiceBusMessageBatch batch = await serviceBusSender.CreateMessageBatchAsync();
+    for (int i = 1; i < maxNumberOfMessages; i++)
+    {
+        if (!batch.TryAddMessage(new ServiceBusMessage($"This is a messgae - {i}")))
+        {
+            Console.WriteLine($"Message - {i} was not added to the batch");
+        }
+        try
+        {
+            await serviceBusSender.SendMessagesAsync(batch);
+            Console.WriteLine("Message Sent");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception: {ex.Message}");
+            throw;
+        }
+        finally
+        {
+            await serviceBusSender.DisposeAsync();
+            await serviceBusClient.DisposeAsync();
+
+        }
+    }
+}
+catch (Exception ex)
+{
+	throw;
+}
---
+++ AzureServiceBus.Subscription/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "put your connection string here";
+
+const string topicName = "your-queuena you can always me-here";
+const string subscriptionName = "your-subscription name";
+
+ServiceBusClient serviceBusClient;
+ServiceBusProcessor processor = default!;
+
+async Task MessageHandler(ProcessMessageEventArgs processMessageEventArgs)
+{
+    string body = processMessageEventArgs.Message.Body.ToString();
+    Console.WriteLine($"{body} - Subscription name: {subscriptionName}");
+    await processMessageEventArgs.CompleteMessageAsync(processMessageEventArgs.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs processMessageEventArgs)
+{
+    Console.WriteLine(processMessageEventArgs.Exception.ToString());
+    return Task.CompletedTask;
+}
+
+serviceBusClient = new ServiceBusClient(serviceBusConnectionString);
+processor = serviceBusClient.CreateProcessor(topicName, subscriptionName, new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+    Console.WriteLine("Press any key to end the process");
+    Console.ReadKey();
+
+    Console.WriteLine("\n Stopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("\n Stopped receiving messages.");
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine($"Exception: {ex.Message}");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await serviceBusClient.DisposeAsync();
+}
